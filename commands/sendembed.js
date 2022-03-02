5fd70f5d83142712077ce08f4981e34fc75e7d12
KuroNeko <kuroinekowitch@gmail.com>
2022-03-01 23:44:32 -0300

    Creado comando para enviar dropdown y continuado concepto de config JSON

--- commands/sendembed.js
+++ commands/sendembed.js
@@ -1,22 +1,37 @@
+const config = require('../config.json');
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 exports.run = (client, message, args) => {
 
-    console.log(client.channels.cache.get(message.channelId));
-    return
+    message.delete();
 
-    const sender = client.guild.channels.cache.get(message.channelId);
+    var categorias = [];
 
-    const row = new MessageActionRow()
-        .addComponents(
-            new MessageSelectMenu()
-                .setCustomId('select')
-                .setPlaceholder('Nothing selected')
-                .addOptions([
-                    { label: 'Select me', description: 'This is a description', value: 'first_option' },
-                    { label: 'You can select me too', description: 'This is also a description', value: 'second_option' }
-                ]),
-        );
+    for(let index = 0; index < config.categories.length; index++) {
+        var data = config.categories[index];
 
-    sender.send({ content: 'Pong!', components: [row] });
+        categorias.push({
+            label: data.name,
+            description: data.desc,
+            value: data.id,
+            emoji: { name: data.emoji.name, id: data.emoji.id }
+        });
+    }
+
+    const row = new MessageActionRow().addComponents(
+        new MessageSelectMenu()
+            .setCustomId('select')
+            .setPlaceholder('Selecciona una categoría')
+            .addOptions(categorias),
+    );
+
+    const embed_content = [{
+        color: 0xcc3366,
+        title: '🎫 Sistema de Soporte',
+        description: 'Para abrir un ticket de soporte, selecciona en la lista de abajo la categoría mas adecuada y nuestro staff te estará atendiendo en la brevedad posible.',
+        footer: config.embed.footer
+    }];
+
+
+    message.channel.send({ embeds: embed_content, components: [row] });
 }
