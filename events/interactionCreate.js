df2df4ffd4669c1dc1ea3fdc95568af6eb1b4ece
KuroNeko <kuroinekowitch@gmail.com>
2022-03-06 18:27:44 -0300

    🧰 Fixed for reset the selectMenu

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -25,13 +25,11 @@ module.exports = {
         // 🥫 Shorthands para SQLite
         interaction.getOpenTicketsOnCat = sql.prepare(" SELECT * FROM tickets WHERE user = ? AND guild = ? AND category = ? AND status = 'A' ");
         interaction.countOpenTicketsOnCat = sql.prepare(" SELECT count(*) as count FROM tickets WHERE user = ? AND guild = ? AND category = ? AND status = 'A' ");
-        interaction.saveNewTicket = sql.prepare(" INSERT INTO tickets (ticket, guild, category, channel, user, timestamp_creation) VALUES (@ticket, @guild, @category, @channel, @user, @timestamp); ");      
+        interaction.saveNewTicket = sql.prepare(" INSERT INTO tickets (ticket, guild, category, channel, user, timestamp_creation) VALUES (@ticket, @guild, @category, @channel, @user, @timestamp); ");
 
         // 📘 Datos Necesarios
         const guild = interaction.guildId;
         const user = interaction.user.id;
-
-        interaction.deferUpdate();
 
         switch(interaction.componentType) {
             case 'SELECT_MENU':
@@ -40,26 +38,53 @@ module.exports = {
                 const category_info = Object.values(config.guilds[guild]).flat().find(r => r.id === menu_id);
                 const total_abierto = interaction.countOpenTicketsOnCat.get(user, guild, menu_id).count;
 
+                const embed_content = [{
+                    color: 0xcc3366,
+                    title: config.embed_content.main_open_ticket.title,
+                    description: config.embed_content.main_open_ticket.description,
+                    footer: config.embed_content.footer
+                }];
+
+                await interaction.deferUpdate();
+
+                await wait(500);
+
+                await interaction.editReply({ embeds: embed_content, ephemeral: true });
+
                 if(total_abierto >= category_info.limit) {
-                    return interaction.reply({ content: "🎫 Has alcanzado el límite de tickets abiertos en esta categoría...", ephemeral: true });
+                    return await interaction.followUp({
+                        content: "🎫 No se puede crear el ticket porque has alcanzado el límite de tickets abiertos en esta categoría...",
+                        ephemeral: true
+                    });
                 }
 
-                await wait(1000); // no funciona...
+                await interaction.followUp({
+                    content: "🎫 El ticket está siendo creado...",
+                    ephemeral: true
+                });
 
-                await interaction.guild.channels.create('ticket_name', {
-                    type: 'text',
-                    parent: category_info.id,
-                    permissionOverwrites: [
+                if(category_info.allowed_staff.length > 0) {
+                    var allowed_staff = [
                         { id: interaction.member.guild.roles.everyone.id, deny: [ 'VIEW_CHANNEL' ] },
                         { id: interaction.guild.members.cache.get(user), allow: [ 'VIEW_CHANNEL', 'SEND_MESSAGES' ] },
                         { id: category_info.allowed_staff, allow: [ 'VIEW_CHANNEL', 'SEND_MESSAGES' ] }
-                    ]
+                    ];
+                } else {
+                    var allowed_staff = [
+                        { id: interaction.member.guild.roles.everyone.id, deny: [ 'VIEW_CHANNEL' ] },
+                        { id: interaction.guild.members.cache.get(user), allow: [ 'VIEW_CHANNEL', 'SEND_MESSAGES' ] }
+                    ];
+                }
+
+                interaction.guild.channels.create('ticket_name', {
+                    type: 'text',
+                    parent: category_info.id,
+                    permissionOverwrites: allowed_staff
                 }).then(async (channel) => {
                     channel.send(`Hi <@${user}>`);
                     // add para guardar en la base de datos
                     // add para que la respuesta sea con embed y botoncitos
                 });
-
             break;
             /* ============================================================================================================================== */
             case 'BUTTON': // Botones de accion (para cerrar el ticket)
