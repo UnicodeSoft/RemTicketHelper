9a4cb0bbc0536658c9aba1845c26980b30fa602b
KuroNeko <kuroinekowitch@gmail.com>
2022-03-11 00:47:37 -0300

    🗑 Eliminado recursos ya no utilizados

--- __borrador/meter botonero en interaction reply.js
+++
@@ -1,9 +0,0 @@
-const row = new MessageActionRow()
-.addComponents(
-    new MessageButton()
-        .setCustomId('primary')
-        .setLabel('Primary')
-        .setStyle('PRIMARY'),
-);
-
-interaction.reply({ content: 'Pong!', components: [row] });
--- commands/ban.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message, args) => {
-    // 🦄 Content of the command
-    message.reply("**Give us nuggots for the confederatiowon!** <:nuggots:864676232737718292><:theiStonks:905161849459314789>");
-}
